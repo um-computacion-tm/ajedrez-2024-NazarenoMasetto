86a1b69e8a43aa80976e1b2f5567b096de76b935
Nazareno Masetto <nazamasetto@gmail.com>
2024-08-14 21:10:15 -0300

    subiendo pisicion inicial de caballo

--- Board.py
+++ Board.py
@@ -1,23 +1,32 @@
-rom rook import Rook
 from pawn import Pawn
+from rook import Rook
+from knight import Knight
+
 
 class Board:
-    def __init__(self):
-        self.__positions__ = [[None for _ in range(8)] for _ in range(8)]
-        self.setup_pieces()
+    def _init_(self):
+        self.grid = [[None for _ in range(8)] for _ in range(8)]
+        self.setup_board()
 
-    def setup_pieces(self):
-        self.__positions__[0][0] = Rook("BLACK")
-        self.__positions__[0][7] = Rook("BLACK")
-        self.__positions__[7][0] = Rook("WHITE")
-        self.__positions__[7][7] = Rook("WHITE")
-            
-        for i in range(8):
-            self.__positions__[1][i] = Pawn("BLACK")
-            self.__positions__[6][i] = Pawn("WHITE")
+    def setup_board(self):
+        
+        for col in range(8):
+            self.grid[1][col] = Pawn("BLACK", f"{col+1}", position=(1, col))
+            self.grid[6][col] = Pawn("WHITE", f"{col+1}", position=(6, col))
 
+        
+        self.grid[0][0] = Rook("BLACK", position=(0, 0))
+        self.grid[0][7] = Rook("BLACK", position=(0, 7))
+        self.grid[7][0] = Rook("WHITE", position=(7, 0))
+        self.grid[7][7] = Rook("WHITE", position=(7, 7))
 
+        
+        self.grid[0][1] = Knight("BLACK", position=(0, 1))
+        self.grid[0][6] = Knight("BLACK", position=(0, 6))
+        self.grid[7][1] = Knight("WHITE", position=(7, 1))
+        self.grid[7][6] = Knight("WHITE", position=(7, 6))
+        
     def get_piece(self, row, col):
-        return self.__positions__[row][col]
+        return self._positions_[row][col]
 
     
