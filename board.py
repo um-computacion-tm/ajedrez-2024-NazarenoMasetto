b5dcb027a8dcfd547476e471cc1ae7d6df984de2
Nazareno Masetto <nazamasetto@gmail.com>
2024-08-30 18:45:13 -0300

    subiendo board actualizado

--- board.py
+++ board.py
@@ -1,49 +1,57 @@
-from rook import *
-from knight import *
-from bishop import *
-from queen import *
-from king import *
-from pawn import *
+from rook import Rook
+from knight import Knight
+from bishop import Bishop
+from queen import Queen
+from king import King
+from pawn import Pawn
+
 class Board:
-    def __init__(self):
-        self.__positions__ = [[None for _ in range(8)] for _ in range(8)]
-        self.__board__ = []
-    def setup_board(self):
-        for col in range(8):
-            self.__positions__[1][col] = Pawn("BLACK", position=(1, col))
-            self.__positions__[6][col] = Pawn("WHITE", position=(6, col))
-        self.__positions__[0][0] = Rook("BLACK")
-        self.__positions__[0][7] = Rook("BLACK")
-        self.__positions__[7][0] = Rook("WHITE")
-        self.__positions__[7][7] = Rook("WHITE")
-        self.__positions__[0][1] = Knight("BLACK")
-        self.__positions__[0][6] = Knight("BLACK")
-        self.__positions__[7][1] = Knight("WHITE")
-        self.__positions__[7][6] = Knight("WHITE")
-        self.__positions__[0][2] = Bishop("BLACK")
-        self.__positions__[0][5] = Bishop("BLACK")
-        self.__positions__[7][2] = Bishop("WHITE")
-        self.__positions__[7][5] = Bishop("WHITE")
-        self.__positions__[0][3] = Queen("BLACK")
-        self.__positions__[7][3] = Queen("WHITE")
-        self.__positions__[0][4] = King("BLACK")
-        self.__positions__[7][4] = King("WHITE")
-    def get_piece(self, row, col):
-        return self.__positions__[row][col]
-    def move_piece(self, from_row, from_col, to_row, to_col):
-        piece = self.get_piece(from_row, from_col)
-        if piece and piece.is_valid_move(self, from_row, from_col, to_row, to_col):
-            self.__positions__[to_row][to_col] = piece
-            self.__positions__[from_row][from_col] = None
-        else: raise ValueError("Movimiento inválido")
-    def show_board(self):
-        # Devuelve una representación textual del tablero
+    def _init_(self):
+        self._positions_ = []
+        for _ in range(8):
+            col = []
+            for _ in range(8):
+                col.append(None)
+            self._positions_.append(col)
+        
+        
+        self._positions_[0][0] = Rook("BLACK")  
+        self._positions_[0][1] = Knight("BLACK")  
+        self._positions_[0][2] = Bishop("BLACK")  
+        self._positions_[0][3] = Queen("BLACK")  
+        self._positions_[0][4] = King("BLACK")  
+        self._positions_[0][5] = Bishop("BLACK")  
+        self._positions_[0][6] = Knight("BLACK")  
+        self._positions_[0][7] = Rook("BLACK")  
+        
+        
+        for i in range(8):
+            self._positions_[1][i] = Pawn("BLACK")
+        
+        
+        self._positions_[7][0] = Rook("WHITE")  
+        self._positions_[7][1] = Knight("WHITE")  
+        self._positions_[7][2] = Bishop("WHITE")  
+        self._positions_[7][3] = Queen("WHITE")  
+        self._positions_[7][4] = King("WHITE")  
+        self._positions_[7][5] = Bishop("WHITE")  
+        self._positions_[7][6] = Knight("WHITE") 
+        self._positions_[7][7] = Rook("WHITE")  
+        
+        
+        for i in range(8):
+            self._positions_[6][i] = Pawn("WHITE")
+
+    def _str_(self):
         board_str = ""
-        for row in self.__positions__:
-            for piece in row:
-                if piece is None:
-                    board_str += ". "
+        for row in self._positions_:
+            for cell in row:
+                if cell is not None:
+                    board_str += str(cell)
                 else:
-                    board_str += piece.symbol + " "
+                    board_str += " "
             board_str += "\n"
         return board_str
+
+    def get_piece(self, row, col):
+        return self._positions_[row][col]
--- test_board.py
+++ test_board.py
@@ -1,96 +1,61 @@
-# import sys
-# import os
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import unittest
-from board import *
-from rook import *
-from knight import *
-from bishop import *
-from queen import *
-from king import *
-from pawn import *
+from king import King 
+from queen import Queen
+from bishop import Bishop
+from rook import Rook
+from knight import Knight
+from pawn import Pawn
+from board import Board
 
-class TestBoardSetup(unittest.TestCase):
-    
-    def setUp(self):
-        self.__board__ = Board()
 
-    def test_initial_piece_positions(self):
-        # Verificar las posiciones iniciales de las piezas
-        self.assertIsInstance(self.__board__.get_piece(0, 0), Rook)
-        self.assertIsInstance(self.__board__.get_piece(0, 1), Knight)
-        self.assertIsInstance(self.__board__.get_piece(0, 2), Bishop)
-        self.assertIsInstance(self.__board__.get_piece(0, 3), Queen)
-        self.assertIsInstance(self.__board__.get_piece(0, 4), King)
-        self.assertIsInstance(self.__board__.get_piece(0, 5), Bishop)
-        self.assertIsInstance(self.__board__.get_piece(0, 6), Knight)
-        self.assertIsInstance(self.__board__.get_piece(0, 7), Rook)
-        self.assertIsInstance(self.__board__.get_piece(7, 0), Rook)
-        self.assertIsInstance(self.__board__.get_piece(7, 1), Knight)
-        self.assertIsInstance(self.__board__.get_piece(7, 2), Bishop)
-        self.assertIsInstance(self.__board__.get_piece(7, 3), Queen)
-        self.assertIsInstance(self.__board__.get_piece(7, 4), King)
-        self.assertIsInstance(self.__board__.get_piece(7, 5), Bishop)
-        self.assertIsInstance(self.__board__.get_piece(7, 6), Knight)
-        self.assertIsInstance(self.__board__.get_piece(7, 7), Rook)
-
-        # Verificar las posiciones iniciales de los peones
-        for col in range(8):
-            self.assertIsInstance(self.board.get_piece(1, col), Pawn)
-            self.assertIsInstance(self.board.get_piece(6, col), Pawn)
 
 class TestBoard(unittest.TestCase):
-    
-    def setUp(self):
-        self.__board__ = Board()
+    def test_init(self):
+        board = Board()
+        self.assertEqual(len(board.Boardpositions_), 8)
+        self.assertEqual(len(board.Boardpositions_[0]), 8)
 
-    
-    def test_move_piece_valid(self):
-        # Mover un peón blanco desde la posición inicial (6, 0) a (5, 0)
-        self.__board__.move_piece(6, 0, 5, 0)
-        move_piece = self.__board__.get_piece(5, 0)
-        self.assertIsNotNone(move_piece)
-        self.assertEqual(move_piece.__color__, "WHITE")
-        self.assertEqual(move_piece.__position__, (5, 0))
-        
-        # Asegurarse de que la posición inicial esté vacía
-        self.assertIsNone(self.__board__.get_piece(6, 0))
-
-    def test_move_piece_invalid(self):
-        # Intento de mover un peón blanco de manera inválida
-        with self.assertRaises(ValueError):
-            self.__board__.move_piece(6, 0, 4, 0)  # Movimiento inválido, ya que debería moverse de a una casilla
-            
     def test_get_piece(self):
-        """Verifica que get_piece devuelve la pieza correcta en una posición dada."""
-        piece = self.__board__.get_piece(0, 0)
-        self.assertIsInstance(piece, Rook)
-        self.assertEqual(piece.__color__, "BLACK")
+        board = Board()
+        self.assertIsInstance(board.get_piece(0, 0), Rook)
+        self.assertIsInstance(board.get_piece(0, 1), Knight)
+        self.assertIsInstance(board.get_piece(0, 2), Bishop)
+        self.assertIsInstance(board.get_piece(0, 3), Queen)
+        self.assertIsInstance(board.get_piece(0, 4), King)
+        self.assertIsInstance(board.get_piece(0, 5), Bishop)
+        self.assertIsInstance(board.get_piece(0, 6), Knight)
+        self.assertIsInstance(board.get_piece(0, 7), Rook)
 
-        piece = self.__board__.get_piece(1, 0)
-        self.assertIsInstance(piece, Pawn)
-        self.assertEqual(piece.__color__, "BLACK")
+        self.assertIsInstance(board.get_piece(7, 0), Rook)
+        self.assertIsInstance(board.get_piece(7, 1), Knight)
+        self.assertIsInstance(board.get_piece(7, 2), Bishop)
+        self.assertIsInstance(board.get_piece(7, 3), Queen)
+        self.assertIsInstance(board.get_piece(7, 4), King)
+        self.assertIsInstance(board.get_piece(7, 5), Bishop)
+        self.assertIsInstance(board.get_piece(7, 6), Knight)
+        self.assertIsInstance(board.get_piece(7, 7), Rook)
 
-        piece = self.__board__.get_piece(7, 7)
-        self.assertIsInstance(piece, Rook)
-        self.assertEqual(piece.__color__, "WHITE")
-        
-    def test_show_board(self):
-        # Verificar que el tablero se muestra correctamente
-        board_str = self.__board__.show_board()
-        expected_row1 = "r . . . . . . r\n"  # Filas con torres negras
-        expected_row2 = "p p p p p p p p\n"  # Filas con peones negros
-        expected_empty = ". . . . . . . .\n"  # Filas vacías
-        expected_row7 = "P P P P P P P P\n"  # Filas con peones blancos
-        expected_row8 = "R . . . . . . R\n"  # Filas con torres blancas
-        
-        
-        self.assertIn(expected_row1, board_str)
-        self.assertIn(expected_row2, board_str)
-        self.assertIn(expected_empty, board_str)
-        self.assertIn(expected_row7, board_str)
-        self.assertIn(expected_row8, board_str)
+        self.assertIsInstance(board.get_piece(1, 0), Pawn)
+        self.assertIsInstance(board.get_piece(1, 1), Pawn)
+        self.assertIsInstance(board.get_piece(1, 2), Pawn)
+        self.assertIsInstance(board.get_piece(1, 3), Pawn)
+        self.assertIsInstance(board.get_piece(1, 4), Pawn)
+        self.assertIsInstance(board.get_piece(1, 5), Pawn)
+        self.assertIsInstance(board.get_piece(1, 6), Pawn)
+        self.assertIsInstance(board.get_piece(1, 7), Pawn)
 
+        self.assertIsInstance(board.get_piece(6, 0), Pawn)
+        self.assertIsInstance(board.get_piece(6, 1), Pawn)
+        self.assertIsInstance(board.get_piece(6, 2), Pawn)
+        self.assertIsInstance(board.get_piece(6, 3), Pawn)
+        self.assertIsInstance(board.get_piece(6, 4), Pawn)
+        self.assertIsInstance(board.get_piece(6, 5), Pawn)
+        self.assertIsInstance(board.get_piece(6, 6), Pawn)
+        self.assertIsInstance(board.get_piece(6, 7), Pawn)
 
-if __name__ == '__main__':
+    def test_str(self):
+        board = Board()
+        self.assertIsInstance(str(board), str)
+
+if Board == '_main_':
     unittest.main()
