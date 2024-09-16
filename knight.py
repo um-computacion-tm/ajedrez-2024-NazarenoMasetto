d7aee6df69ff853ceab7e1249e3094e6a062df28
Nazareno Masetto <nazamasetto@gmail.com>
2024-09-16 18:59:22 -0300

    Modificando knight

--- knight.py
+++ knight.py
@@ -1,5 +1,8 @@
 from piece import Piece
 
 class Knight(Piece):
-    def __init__(self, color, position=None):
-        super().__init__(color, position)
+    def valid_moves(self, position, board):
+        row, col = position
+        moves = [(row + 2, col + 1), (row + 2, col - 1), (row - 2, col + 1), (row - 2, col - 1),
+                 (row + 1, col + 2), (row + 1, col - 2), (row - 1, col + 2), (row - 1, col - 2)]
+        return [(r, c) for r, c in moves if 0 <= r < 8 and 0 <= c < 8]
