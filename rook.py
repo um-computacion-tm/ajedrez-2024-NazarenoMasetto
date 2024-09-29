cdface125f347359bd5c4c43f0068f89b57c1db5
Nazareno Masetto <nazamasetto@gmail.com>
2024-09-29 13:02:20 -0300

    Mejorando clase rook

--- rook.py
+++ rook.py
@@ -1,69 +1,36 @@
 from piece import Piece
-
 class Rook(Piece):
-    white_str = "♜"
-    black_str = "♖"
+    def __init__(self, color):
+       
+        super().__init__(color)
 
-    def valid_positions(
-        self,
-        from_row,
-        from_col,
-        to_row,
-        to_col,
-    ):
-        possible_positions = (
-            self.possible_positions_vd(from_row, from_col) +
-            self.possible_positions_va(from_row, from_col) +
-            self.possible_positions_hd(from_row, from_col) +
-            self.possible_positions_ha(from_row, from_col)
-        )
-        return (to_row, to_col) in possible_positions
-
-    def possible_positions_vd(self, row, col):
+    def valid_moves(self, current_position, board):
         
-        possibles = []
-        for next_row in range(row + 1, 8):
-            other_piece = self._board_.get_piece(next_row, col)
-            if other_piece is not None:
-                if other_piece._color_ != self._color_:
-                    possibles.append((next_row, col))
-                break
-            possibles.append((next_row, col))
-        return possibles
-
-    def possible_positions_va(self, row, col):
-        
-        possibles = []
-        for next_row in range(row - 1, -1, -1):
-            other_piece = self._board_.get_piece(next_row, col)
-            if other_piece is not None:
-                if other_piece._color_ != self._color_:
-                    possibles.append((next_row, col))
-                break
-            possibles.append((next_row, col))
-        return possibles
-
-    def possible_positions_hd(self, row, col):
-        
-        possibles = []
-        for next_col in range(col + 1, 8):
-            other_piece = self._board_.get_piece(row, next_col)
-            if other_piece is not None:
-                if other_piece._color_ != self._color_:
-                    possibles.append((row, next_col))
-                break
-            possibles.append((row, next_col))
-        return possibles
-
-    def possible_positions_ha(self, row, col):
+        row, col = current_position
+        moves = []
         
         
-        possibles = []
-        for next_col in range(col - 1, -1, -1):
-            other_piece = self._board_.get_piece(row, next_col)
-            if other_piece is not None:
-                if other_piece._color_ != self._color_:
-                    possibles.append((row, next_col))
-                break
-            possibles.append((row, next_col))
-        return possibles
+        directions = [
+            (-1, 0), (1, 0),  
+            (0, -1), (0, 1)   
+        ]
+
+        for dr, dc in directions:
+            new_row, new_col = row + dr, col + dc
+           
+            while 0 <= new_row < 8 and 0 <= new_col < 8:
+                if board[new_row][new_col] == " ":
+                    moves.append((new_row, new_col)) 
+                elif board[new_row][new_col].get_color() != self.get_color():
+                    moves.append((new_row, new_col))  
+                    break 
+                else:
+                    break  
+                new_row += dr
+                new_col += dc
+
+        return moves
+
+    def get_symbol(self):
+       
+        return "R" if self.get_color() == 'White' else "r"
