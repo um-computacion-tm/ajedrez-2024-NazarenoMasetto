b6bf6aea92d17caa598645874c31efd1177576bb
Nazareno Masetto <nazamasetto@gmail.com>
2024-09-29 20:14:00 -0300

    corrigiendo bishop

--- bishop.py
+++ bishop.py
@@ -11,24 +11,41 @@ class Bishop(Piece):
         ])
 
     def _get_moves_in_directions(self, current_position, board, directions):
+        moves = []
+        for dr, dc in directions:
+            moves.extend(self._explore_direction(current_position, board, dr, dc))
+        return moves
+
+    def _explore_direction(self, current_position, board, dr, dc):
+        """Explora una dirección específica para encontrar los movimientos válidos."""
         row, col = current_position
+        new_row, new_col = row + dr, col + dc
         moves = []
         
-        for dr, dc in directions:
-            new_row, new_col = row + dr, col + dc
-            
-            while 0 <= new_row < 8 and 0 <= new_col < 8:
-                if board[new_row][new_col] == " ":
-                    moves.append((new_row, new_col))
-                elif board[new_row][new_col].get_color() != self.get_color():
-                    moves.append((new_row, new_col))
-                    break  
-                else:
-                    break  
-                new_row += dr
-                new_col += dc
-                
+        while self._is_within_bounds(new_row, new_col):
+            if self._is_empty(board, new_row, new_col):
+                moves.append((new_row, new_col))
+            elif self._is_opponent_piece(board, new_row, new_col):
+                moves.append((new_row, new_col))
+                break
+            else:
+                break
+            new_row += dr
+            new_col += dc
+
         return moves
+
+    def _is_within_bounds(self, row, col):
+        """Verifica si una posición está dentro del tablero."""
+        return 0 <= row < 8 and 0 <= col < 8
+
+    def _is_empty(self, board, row, col):
+        """Verifica si una casilla está vacía."""
+        return board[row][col] == " "
+
+    def _is_opponent_piece(self, board, row, col):
+        """Verifica si una casilla contiene una pieza enemiga."""
+        return board[row][col] != " " and board[row][col].get_color() != self.get_color()
 
     def get_symbol(self):
         return "B" if self.get_color() == 'White' else "b"
