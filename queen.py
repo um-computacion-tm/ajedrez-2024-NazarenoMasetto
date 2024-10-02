23ec96c656ac93cd756b97cf43ad6e862a6138b6
Nazareno Masetto <nazamasetto@gmail.com>
2024-10-02 10:58:01 -0300

    mejorando queen

--- queen.py
+++ queen.py
@@ -8,7 +8,7 @@ class Queen(Piece):
         row, col = current_position
         moves = []
 
-        # Definimos las direcciones de movimiento de la reina
+      
         directions = [
             (-1, 0), (1, 0),   # Vertical
             (0, -1), (0, 1),   # Horizontal
@@ -18,29 +18,39 @@ class Queen(Piece):
 
         # Recorremos todas las direcciones y añadimos los movimientos válidos
         for direction in directions:
-            moves.extend(self._traverse_direction(current_position, direction, board))
+            moves.extend(self._collect_moves_in_direction(current_position, direction, board))
 
         return moves
 
-    def _traverse_direction(self, current_position, direction, board):
+    def _collect_moves_in_direction(self, current_position, direction, board):
+        """Función que recorre en una dirección y agrega movimientos válidos."""
         moves = []
+        for new_pos in self._generate_positions_in_direction(current_position, direction):
+            if self._is_valid_move(new_pos, board):
+                moves.append(new_pos)
+            if not self._can_continue(new_pos, board):
+                break
+        return moves
+
+    def _generate_positions_in_direction(self, current_position, direction):
+        """Genera todas las posiciones en una dirección dada a partir de la posición actual."""
         row, col = current_position
         dr, dc = direction
         new_row, new_col = row + dr, col + dc
-
-        # Recorremos el tablero en la dirección dada
         while 0 <= new_row < 8 and 0 <= new_col < 8:
-            if board[new_row][new_col] == " ":
-                moves.append((new_row, new_col))
-            elif board[new_row][new_col].get_color() != self.get_color():
-                moves.append((new_row, new_col))  # Captura válida
-                break
-            else:
-                break
+            yield (new_row, new_col)
             new_row += dr
             new_col += dc
-        
-        return moves
+
+    def _is_valid_move(self, position, board):
+        """Comprueba si la posición es un movimiento válido (casilla vacía o captura)."""
+        row, col = position
+        return board[row][col] == " " or board[row][col].get_color() != self.get_color()
+
+    def _can_continue(self, position, board):
+        """Determina si podemos continuar recorriendo después de una posición."""
+        row, col = position
+        return board[row][col] == " "
 
     def get_symbol(self):
         return "Q" if self.get_color() == 'White' else "q"
