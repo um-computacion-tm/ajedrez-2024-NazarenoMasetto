02f383c7e44677ca342a906a003dd30f8d19aac4
Nazareno Masetto <nazamasetto@gmail.com>
2024-10-02 10:48:39 -0300

    corrigiendo clase queen

--- queen.py
+++ queen.py
@@ -1,38 +1,41 @@
 from piece import Piece
+
 class Queen(Piece):
     def __init__(self, color):
-        
         super().__init__(color)
 
     def valid_moves(self, current_position, board):
-       
         row, col = current_position
         moves = []
-        
-        
+
         directions = [
-            (-1, 0), (1, 0),  
-            (0, -1), (0, 1),  
-            (-1, -1), (-1, 1),  
-            (1, -1), (1, 1)    
+            (-1, 0), (1, 0),   # Vertical
+            (0, -1), (0, 1),   # Horizontal
+            (-1, -1), (-1, 1), # Diagonal superior
+            (1, -1), (1, 1)    # Diagonal inferior
         ]
 
         for dr, dc in directions:
-            new_row, new_col = row + dr, col + dc
-            
-            while 0 <= new_row < 8 and 0 <= new_col < 8:
-                if board[new_row][new_col] == " ":
-                    moves.append((new_row, new_col))  
-                elif board[new_row][new_col].get_color() != self.get_color():
-                    moves.append((new_row, new_col))  # Captura válida
-                    break  
-                else:
-                    break  
-                new_row += dr
-                new_col += dc
+            moves.extend(self._traverse_direction(row, col, dr, dc, board))
 
         return moves
 
+    def _traverse_direction(self, row, col, dr, dc, board):
+        moves = []
+        new_row, new_col = row + dr, col + dc
+        
+        while 0 <= new_row < 8 and 0 <= new_col < 8:
+            if board[new_row][new_col] == " ":
+                moves.append((new_row, new_col))
+            elif board[new_row][new_col].get_color() != self.get_color():
+                moves.append((new_row, new_col))
+                break
+            else:
+                break
+            new_row += dr
+            new_col += dc
+        
+        return moves
+
     def get_symbol(self):
-       
         return "Q" if self.get_color() == 'White' else "q"
