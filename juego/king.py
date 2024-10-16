288beb20f7c77964bb459ea62966d6b1da684d84
Nazareno Masetto <nazamasetto@gmail.com>
2024-10-16 11:22:36 -0300

    mejorando king y knigth

--- juego/king.py
+++ juego/king.py
@@ -1,20 +1,14 @@
-from juego.piece import Piece
-
 class King(Piece):
     def __init__(self, color):
         super().__init__(color)
 
     def valid_moves(self, current_position, board):
         row, col = current_position
-        directions = self._get_king_directions()  # Obtiene los movimientos del rey
-        return self._get_valid_moves(row, col, directions, board)
-
-    def get_symbol(self):
-        return "K" if self.get_color() == 'White' else "k"
-
-    def _get_king_directions(self):
-        # Movimientos del rey en las ocho direcciones
-        return [
+        king_moves = [
             (-1, 0), (1, 0), (0, -1), (0, 1),  # Vertical y horizontal
             (-1, -1), (-1, 1), (1, -1), (1, 1)  # Diagonales
         ]
+        return self._get_valid_moves(row, col, king_moves, board)
+
+    def get_symbol(self):
+        return "K" if self.get_color() == 'White' else "k"
--- juego/knight.py
+++ juego/knight.py
@@ -1,20 +1,14 @@
-from juego.piece import Piece
-
 class Knight(Piece):
     def __init__(self, color):
         super().__init__(color)
 
     def valid_moves(self, current_position, board):
         row, col = current_position
-        knight_moves = self._get_knight_moves()  # Obtiene los movimientos del caballo
+        knight_moves = [
+            (2, 1), (2, -1), (-2, 1), (-2, -1),
+            (1, 2), (1, -2), (-1, 2), (-1, -2)
+        ]
         return self._get_valid_moves(row, col, knight_moves, board)
 
     def get_symbol(self):
         return "N" if self.get_color() == 'White' else "n"
-
-    def _get_knight_moves(self):
-        # Movimientos en forma de L del caballo
-        return [
-            (2, 1), (2, -1), (-2, 1), (-2, -1),
-            (1, 2), (1, -2), (-1, 2), (-1, -2)
-        ]
