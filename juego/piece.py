9cdd0060cf6f285f3f764b86adced351bfd2d66e
Nazareno Masetto <nazamasetto@gmail.com>
2024-10-16 12:30:32 -0300

    mejorando rook

--- juego/piece.py
+++ juego/piece.py
@@ -20,12 +20,31 @@ class Piece:
             new_row, new_col = row + dr, col + dc
             if self._is_within_board(new_row, new_col):
                 target_square = board[new_row][new_col]
-                if target_square == " " or target_square.get_color() != self.get_color():
+                if self._is_target_valid(target_square):
                     valid_moves.append((new_row, new_col))
         return valid_moves
 
+    def _is_target_valid(self, target_square):
+        return target_square == " " or target_square.get_color() != self.get_color()
+
     def _is_within_board(self, row, col):
         return 0 <= row < 8 and 0 <= col < 8
+
+    def _explore_direction(self, row, col, direction, board):
+        valid_moves = []
+        dr, dc = direction
+        for i in range(1, 8):
+            new_row, new_col = row + dr * i, col + dc * i
+            if not self._is_within_board(new_row, new_col):
+                break
+            target_square = board[new_row][new_col]
+            if self._is_target_valid(target_square):
+                valid_moves.append((new_row, new_col))
+                if target_square != " ":
+                    break
+            else:
+                break
+        return valid_moves
 
     @staticmethod
     def get_knight_moves():
@@ -41,20 +60,3 @@ class Piece:
             (0, -1), (0, 1),  # Horizontal
             (-1, -1), (-1, 1), (1, -1), (1, 1)  # Diagonal
         ]
-
-    def _explore_direction(self, row, col, direction, board):
-        valid_moves = []
-        dr, dc = direction
-        for i in range(1, 8):
-            new_row, new_col = row + dr * i, col + dc * i
-            if not self._is_within_board(new_row, new_col):
-                break
-            target_square = board[new_row][new_col]
-            if target_square == " ":
-                valid_moves.append((new_row, new_col))
-            elif target_square.get_color() != self.get_color():
-                valid_moves.append((new_row, new_col))
-                break
-            else:
-                break
-        return valid_moves
--- juego/rook.py
+++ juego/rook.py
@@ -5,47 +5,14 @@ class Rook(Piece):
         super().__init__(color)
 
     def valid_moves(self, current_position, board):
-        """
-        Calcula los movimientos válidos de la torre según las reglas del ajedrez.
-        """
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Movimientos verticales y horizontales
         return self._generate_linear_moves(current_position, board, directions)
 
     def _generate_linear_moves(self, current_position, board, directions):
-        """
-        Función auxiliar para generar movimientos lineales (como los de torre y alfil).
-        """
         moves = []
-
         for direction in directions:
-            moves.extend(self._explore_direction(current_position, direction, board))
-
-        return moves
-
-    def _explore_direction(self, current_position, direction, board):
-        """
-        Explora una dirección específica hasta que se encuentre un borde del tablero o una pieza.
-        """
-        row, col = current_position
-        dr, dc = direction
-        moves = []
-
-        new_row, new_col = row + dr, col + dc
-        
-        while self.is_within_bounds(new_row, new_col):  
-            piece = board[new_row][new_col]
-            if piece == " ":
-                moves.append((new_row, new_col))  
-            elif piece.get_color() != self.get_color():
-                moves.append((new_row, new_col))  
-                break  
-            else:
-                break  
-            new_row += dr
-            new_col += dc
-
+            moves.extend(self._explore_direction(current_position[0], current_position[1], direction, board))
         return moves
 
     def get_symbol(self):
-        """Devuelve el símbolo que representa la torre en el tablero."""
         return "R" if self.get_color() == 'White' else "r"
