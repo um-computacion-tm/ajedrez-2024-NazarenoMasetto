0ef7378bdd14909d3ead6f266573506578e07583
Nazareno Masetto <nazamasetto@gmail.com>
2024-10-16 11:30:56 -0300

    mejorando knigth y king

--- juego/king.py
+++ juego/king.py
@@ -1,3 +1,4 @@
+from juego.piece import Piece
 class King(Piece):
     def __init__(self, color):
         super().__init__(color)
--- juego/knight.py
+++ juego/knight.py
@@ -1,3 +1,4 @@
+from juego.piece import Piece
 class Knight(Piece):
     def __init__(self, color):
         super().__init__(color)
@@ -8,7 +9,15 @@ class Knight(Piece):
             (2, 1), (2, -1), (-2, 1), (-2, -1),
             (1, 2), (1, -2), (-1, 2), (-1, -2)
         ]
-        return self._get_valid_moves(row, col, knight_moves, board)
+        valid_moves = []
+        for move in knight_moves:
+            new_row, new_col = row + move[0], col + move[1]
+            if self._is_within_board(new_row, new_col, board):
+                valid_moves.append((new_row, new_col))
+        return valid_moves
 
     def get_symbol(self):
         return "N" if self.get_color() == 'White' else "n"
+
+    def _is_within_board(self, row, col, board):
+        return 0 <= row < len(board) and 0 <= col < len(board[0])
