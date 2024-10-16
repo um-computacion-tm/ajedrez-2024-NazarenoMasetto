f672b0deddfc69ef840f5a24e83483fdbc0dcae1
Nazareno Masetto <nazamasetto@gmail.com>
2024-10-16 11:00:37 -0300

    mejorando knitgh y king

--- juego/king.py
+++ juego/king.py
@@ -6,8 +6,8 @@ class King(Piece):
 
     def valid_moves(self, current_position, board):
         row, col = current_position
-        directions = self._get_king_directions()  # Obtenemos las direcciones del rey
-        return self._get_valid_king_moves(row, col, directions, board)
+        directions = self._get_king_directions()
+        return self._get_valid_moves(row, col, directions, board)
 
     def get_symbol(self):
         return "K" if self.get_color() == 'White' else "k"
@@ -17,13 +17,4 @@ class King(Piece):
             (-1, 0), (1, 0), (0, -1), (0, 1),  # Vertical y horizontal
             (-1, -1), (-1, 1), (1, -1), (1, 1)  # Diagonales
         ]
-
-    def _get_valid_king_moves(self, row, col, directions, board):
-        valid_moves = []
-        for dr, dc in directions:
-            new_row, new_col = row + dr, col + dc
-            # Usamos _is_within_board heredado de Piece
-            if self._is_within_board(new_row, new_col) and (board[new_row][new_col] == " " or board[new_row][new_col].get_color() != self.get_color()):
-                valid_moves.append((new_row, new_col))
-        return valid_moves
 
--- juego/knight.py
+++ juego/knight.py
@@ -6,8 +6,8 @@ class Knight(Piece):
 
     def valid_moves(self, current_position, board):
         row, col = current_position
-        knight_moves = self._get_knight_moves()  # Usamos una función para obtener los movimientos de un caballo
-        return self._get_valid_knight_moves(row, col, knight_moves, board)
+        knight_moves = self._get_knight_moves()
+        return self._get_valid_moves(row, col, knight_moves, board)
 
     def get_symbol(self):
         return "N" if self.get_color() == 'White' else "n"
@@ -17,12 +17,3 @@ class Knight(Piece):
             (2, 1), (2, -1), (-2, 1), (-2, -1),
             (1, 2), (1, -2), (-1, 2), (-1, -2)
         ]
-
-    def _get_valid_knight_moves(self, row, col, knight_moves, board):
-        valid_moves = []
-        for dr, dc in knight_moves:
-            new_row, new_col = row + dr, col + dc
-            # Usamos _is_within_board heredado de Piece
-            if self._is_within_board(new_row, new_col) and (board[new_row][new_col] == " " or board[new_row][new_col].get_color() != self.get_color()):
-                valid_moves.append((new_row, new_col))
-        return valid_moves
