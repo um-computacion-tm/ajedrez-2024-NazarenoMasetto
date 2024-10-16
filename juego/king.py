aafa8b7c1b32ad1d5a6a01e706f8057830f5d811
Nazareno Masetto <nazamasetto@gmail.com>
2024-10-16 11:32:43 -0300

    mejorando king

--- juego/king.py
+++ juego/king.py
@@ -9,7 +9,15 @@ class King(Piece):
             (-1, 0), (1, 0), (0, -1), (0, 1),  # Vertical y horizontal
             (-1, -1), (-1, 1), (1, -1), (1, 1)  # Diagonales
         ]
-        return self._get_valid_moves(row, col, king_moves, board)
+        valid_moves = []
+        for move in king_moves:
+            new_row, new_col = row + move[0], col + move[1]
+            if self._is_within_board(new_row, new_col, board):
+                valid_moves.append((new_row, new_col))
+        return valid_moves
 
     def get_symbol(self):
         return "K" if self.get_color() == 'White' else "k"
+
+    def _is_within_board(self, row, col, board):
+        return 0 <= row < len(board) and 0 <= col < len(board[0])
