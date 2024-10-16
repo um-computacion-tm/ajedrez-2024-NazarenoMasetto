175d9a055aec2c413c76bb4d91407b94514dd686
Nazareno Masetto <nazamasetto@gmail.com>
2024-10-16 12:01:05 -0300

    mejorando knith y king

--- juego/knight.py
+++ juego/knight.py
@@ -1,23 +1,19 @@
 from juego.piece import Piece
+
 class Knight(Piece):
     def __init__(self, color):
         super().__init__(color)
 
     def valid_moves(self, current_position, board):
+        knight_moves = self._get_knight_moves()  # Usamos una función para obtener los movimientos de un caballo
         row, col = current_position
-        knight_moves = [
-            (2, 1), (2, -1), (-2, 1), (-2, -1),
-            (1, 2), (1, -2), (-1, 2), (-1, -2)
-        ]
-        valid_moves = []
-        for move in knight_moves:
-            new_row, new_col = row + move[0], col + move[1]
-            if self._is_within_board(new_row, new_col, board):
-                valid_moves.append((new_row, new_col))
-        return valid_moves
+        return self._get_valid_moves(row, col, knight_moves, board)
 
     def get_symbol(self):
         return "N" if self.get_color() == 'White' else "n"
 
-    def _is_within_board(self, row, col, board):
-        return 0 <= row < len(board) and 0 <= col < len(board[0])
+    def _get_knight_moves(self):
+        return [
+            (2, 1), (2, -1), (-2, 1), (-2, -1),
+            (1, 2), (1, -2), (-1, 2), (-1, -2)
+        ]
