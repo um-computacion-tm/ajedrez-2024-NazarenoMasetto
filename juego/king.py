ad8e0ae1ec2c52592cb7b864b893ca885eae4609
Nazareno Masetto <nazamasetto@gmail.com>
2024-10-18 11:37:53 -0300

    cambiando clase king

--- juego/king.py
+++ juego/king.py
@@ -7,20 +7,10 @@ class King(Piece):
         return 'K'
 
     def valid_moves(self, current_position, board):
-        possible_moves = self._get_king_moves()
-        valid_moves = []
-
-        for move in possible_moves:
-            new_row, new_col = self._calculate_new_position(current_position, move)
-            if self._is_within_board(new_row, new_col) and self._can_move_to(new_row, new_col, board):
-                valid_moves.append((new_row, new_col))
-
-        return valid_moves
+        possible_moves = self._get_moves([(0, 1), (1, 0), (0, -1), (-1, 0),
+                                           (1, 1), (1, -1), (-1, 1), (-1, -1)])
+        return self.calculate_valid_moves(current_position, possible_moves, board)
 
     @staticmethod
-    def _get_king_moves():
-        return [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-
-    def _can_move_to(self, row, col, board):
-        target_piece = board[row][col]
-        return target_piece is None or target_piece.get_color() != self.get_color()
+    def _get_moves(directions):
+        return directions
