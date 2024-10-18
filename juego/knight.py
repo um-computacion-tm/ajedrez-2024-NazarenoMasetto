da1fa8faea1a6733efac359865b09605472638f8
Nazareno Masetto <nazamasetto@gmail.com>
2024-10-18 11:05:32 -0300

    cambiando knigth

--- juego/knight.py
+++ juego/knight.py
@@ -1,13 +1,17 @@
 from juego.piece import Piece
 
 class Knight(Piece):
-    def __init__(self, color):
-        super().__init__(color)
+    
+    def valid_positions(self, from_row, from_col, to_row, to_col):
+        
+        directions = self.generate_knight_directions()  
+        possible_positions = self.possible_moves_general(from_row, from_col, directions, single_step=True)
+        return (to_row, to_col) in possible_positions
 
-    def valid_moves(self, current_position, board):
-        row, col = current_position
-        knight_moves = self.get_knight_moves()
-        return self._get_valid_moves(row, col, knight_moves, board)
-
+    def generate_knight_directions(self):
+       
+        moves = [2, 1, -1, -2]
+        return [(i, j) for i in moves for j in moves if abs(i) != abs(j)]
+    
     def get_symbol(self):
-        return "N" if self.get_color() == 'White' else "n"
+        return "K" if self.get_color() == 'White' else "k"
